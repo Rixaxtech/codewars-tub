191a06fa01f481a27120f2fd207aaaa1da731aeb
Enrica Berko <enrica.berko@gmail.com>
2022-11-19 17:29:56 -0500

    coding challenge 11/19/22

---
+++ interview-prep.js
@@ -0,0 +1,23 @@
+//function word say is a palindrome
+
+// word -> str, no empty, no !, no nums, no funny biz, no caps, 1 word
+//return true || false
+
+// racecar -> true
+// bob -> true
+// tacos -> false
+
+function isPalindrome(word){
+    //word -> reverse that word - split reverse join
+    let reversedWord = word.split('').reverse().join('')
+    //revWord === word
+    if (word === reversedWord){
+        return true
+    }else{
+        return false
+    }
+}
+
+console.log(isPalindrome('racecar'), 'true')
+console.log(isPalindrome('bob'), 'true')
+console.log(isPalindrome('tacos'), 'false')
